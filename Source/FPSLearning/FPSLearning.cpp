b19a1c275d9111e81e3afaab8f29b49beb6f97a1
TDKArkham <LCMTDK117@gmail.com>
2023-02-18 14:44:57 +0800

    Repository initialized.

---
+++ Source/FPSLearning/FPSLearning.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSLearning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSLearning, "FPSLearning" );
---
+++ Source/FPSLearning/FPSLearningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSLearningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSLEARNING_API AFPSLearningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
